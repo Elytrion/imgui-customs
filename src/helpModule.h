386a37dec5bc63ee07a9b724fcba63b6b9436379
Jed Goh <electronfirework@gmail.com>
2026-01-27 14:45:37 +0800

    Expand user guide in HelpModule demo
    Replaced placeholder text with detailed bullet points explaining how to use the module showcases and demo panels. The updated guide provides clearer instructions for users on navigating and utilizing the modules.

--- src/helpModule.h
+++ src/helpModule.h
@@ -25,8 +25,22 @@ void HelpModule::OnPreSelectable()
 void HelpModule::DrawSelectedDemo()
 {
 	ImGui::SeparatorText("USER GUIDE:");
-	DrawPlaceholderText();
-	ImGui::SeparatorEx(ImGuiSeparatorFlags_Horizontal, 3.0f);
+	ImGui::BulletText("Click on the title bars to open and collapse the module showcases.");
+	ImGui::BulletText("Each module here showcases a custom collection of header-only ImGui widgets and helpers.");
+	ImGui::BulletText("Each module (demo and headers) are contained in individual folders in this projects src folder.");
+	ImGui::BulletText("Each module consists of a showcase and demo panel.");
+	ImGui::Indent();
+	ImGui::BulletText("The showcase is embedded in this main window and is a small demonstration of the module widgets.");
+	ImGui::BulletText("The demo panel is a separate window that can be opened by clicking the 'Open Demo Panel' button.");
+	ImGui::BulletText("The demo panel contains a more complete demonstration of the module widgets.");
+	ImGui::BulletText("It typically includes customisation options and additional examples.");
+	ImGui::BulletText("You can open multiple demo panels at once");
+	ImGui::Unindent();
+	ImGui::BulletText("Feel free to undock this main window to have more space for the demo panels.");
+	ImGui::BulletText("Each module is independent and can be extracted and used in your own projects.");
+	ImGui::BulletText("You can do this by simply including the relevant header files from the module folder.");
+	ImGui::BulletText("No additional setup or configuration is required.");
+	ImGui::SeparatorEx(ImGuiSeparatorFlags_Horizontal, 2.0f);
 }
 void HelpModule::OnPostSelectable()
 {
