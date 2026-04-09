e4f6282649f8c799b70efaa24ab4101801893132
Jed Goh <electronfirework@gmail.com>
2026-04-09 17:34:19 +0800

    Add BCFDocument type and Parse stub
    Introduce BCFDocument and nested BCFTopicEntry to represent BCF package contents (version doc, optional extensions/documents/project, and topic entries with markup, viewpoints and snapshot names). Add ParseConfig and a static BCFIO::Parse declaration (with default cfg) in the header, include the XMLParser in the cpp and add an empty Parse implementation stub. This sets up the data model and entry point for future XML parsing work.

--- src/bcf-lib/BCFIO.cpp
+++ src/bcf-lib/BCFIO.cpp
@@ -1,1 +1,7 @@
 #include "BCFIO.h"
+#include "xmllib/XMLParser.h"
+
+BCFDocument BCFIO::Parse(const std::string& xmlFilePath, std::string& errMsg, const ParseConfig& cfg)
+{
+
+}
--- src/bcf-lib/BCFIO.h
+++ src/bcf-lib/BCFIO.h
@@ -1,7 +1,30 @@
 #pragma once
+#include "BCFDocumentStore.hpp"
+#include <optional>
+#include <vector>
+
+struct BCFDocument 
+{
+	DocumentRef versionDoc;						// .version file, required
+	std::optional<DocumentRef> extensionsDoc;	// .xml file, optional, defines the extensions of a project
+	std::optional<DocumentRef> documentsDoc;	// .xml file, optional, defines any additional documents in a project
+	std::optional<DocumentRef> projectDoc;		// .bcfp file, optional, defines details of a project
+	struct BCFTopicEntry
+	{
+		std::string guid;						// name of the folder is always the guid of the topic
+		DocumentRef markupDoc;					// .bcf file, required
+		std::vector<DocumentRef> viewpointDoc;  // .bcfv files, can be multiple or none
+		std::vector<std::string> snapshotNames; // file names of the snapshots in the topic folder, can be multiple or none
+	};
+	std::vector<BCFTopicEntry> topics;
+};
 
 
 struct BCFIO
 {
-
+	struct ParseConfig
+	{
+		// TODO
+	};
+	static BCFDocument Parse(const std::string& xmlFilePath, std::string& errMsg, const ParseConfig& cfg = {});
 };
