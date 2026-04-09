c05a9f48edda86c0b30f550f8368104fa3b95938
Jed Goh <electronfirework@gmail.com>
2026-04-09 16:01:21 +0800

    Removed BCFRaw
    Remove the BCFRaw struct and associated includes (pugixml, optional, vector, string) from BCFIO.h and clear the LoadBCF declaration. This refactors the header to eliminate internal data-type definitions and external dependencies, decoupling implementation details from the public header. Callers will need to adapt to the updated API/implementation location for BCF loading.

--- src/bcf-lib/BCFIO.h
+++ src/bcf-lib/BCFIO.h
@@ -1,23 +1,7 @@
 #pragma once
-#include "pugixml.hpp"
-#include <optional>
-#include <vector>
 
-struct BCFRaw
-{
-	pugi::xml_document version; // .version file, required
-	std::optional<pugi::xml_document> projectXML; // .bcfp file, optional
-	struct BCFTopicEntry
-	{
-		std::string guid;
-		pugi::xml_document markupXML;
-		std::vector<pugi::xml_document> viewpointXMLs;
-		std::vector<std::string> snapshotRefs; // file names of the snapshots in the topic folder
-	};
-	std::vector<BCFTopicEntry> topics;
-};
 
 struct BCFIO
 {
-	static BCFRaw LoadBCF(const std::string& filePath, std::string& errMsg);
+
 };
